35ee8a184ac1da336a934743b94031053fd6a32c
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-10 20:54:03 +0200

    Passed task 9.

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor());
+
+const bodyREF = document.querySelector('body')
+// console.log(bodyREF);
+const butREF = document.querySelector('.change-color');
+const spanREF = document.querySelector('.color')
+const img = document.querySelector('.random__img')
+
+bodyREF.addEventListener('click', () => {
+  bodyREF.style.backgroundColor = getRandomHexColor();
+  spanREF.textContent = getRandomHexColor();  
+  
+})
+
+
+ 
