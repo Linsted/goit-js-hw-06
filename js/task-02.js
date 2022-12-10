a2cdfb4ad82b61c3fb496eac595084d6d91c1333
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-10 12:46:03 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients')
+
+const elements = ingredients.map(el => {
+  const itemEl = document.createElement(`li`);
+  itemEl.classList.add('item')
+  itemEl.textContent = el;
+  return itemEl;
+})
+console.log(elements);
+ 
+
+listEl.append(...elements)
+
+
+
+
+
+
+
+
+
