9b8af7b2dde931de480ff515a4f21086a1b5692b
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-10 20:10:28 +0200

    Passed task 8.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formREF = document.querySelector(`.login-form`);
+formREF.addEventListener('submit', validForm)
+const obj = {};
+function validForm(event) {
+    event.preventDefault();
+    if (event.currentTarget.elements.email.value === '' || event.currentTarget.elements.password.value === '')
+    { alert('Dear User, please fill in all fields!') }
+    else {
+    const { elements: { email, password } } = event.currentTarget;
+    obj.email = email.value;
+    obj.password = password.value;
+    console.log(obj);
+    event.currentTarget.reset();
+    }
+}
+
+
 
