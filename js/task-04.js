e7464993b5b7740038ef0d60e8c29cae009c3108
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-10 16:17:51 +0200

    Passed task 4.

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,16 @@
-const counterValue = 
+let counterValue = 0;
+const counterREF = document.querySelector('#value')
+
+const incrButtonREF = document.querySelector('[data-action="increment"]')
+const dectButtonREF = document.querySelector('[data-action="decrement"]')
+
+incrButtonREF.addEventListener('click', () => {
+    counterValue += 1;
+    // console.log(counterValue);
+    counterREF.textContent = counterValue;
+})
+dectButtonREF.addEventListener('click', () => {
+    counterValue -= 1;
+    // console.log(counterValue);
+    counterREF.textContent = counterValue;
+})
