d185fc9e97580a229db73ee37520c070e351e4a7
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-10 20:55:32 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,12 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-console.log(getRandomHexColor());
 
 const bodyREF = document.querySelector('body')
 const butREF = document.querySelector('.change-color');
 const spanREF = document.querySelector('.color')
-
 
 bodyREF.addEventListener('click', () => {
   bodyREF.style.backgroundColor = getRandomHexColor();
