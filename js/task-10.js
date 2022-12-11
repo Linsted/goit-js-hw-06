fbbce337367934bc2eb72eaaf9984e0ba6d441d5
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-11 15:11:12 +0200

    Passed task 10.

--- js/task-10.js
+++ js/task-10.js
@@ -13,36 +13,32 @@ const destroyButREF = document.querySelector('[data-destroy]')
 // Змінні////
 let divWidth = 30;
 let divHeight = 30;
-const arr = []
+const arr = [];
 let string = '';
 
 
 //Eventlestener
 createButREF.addEventListener('click', () => {
-
   string = string.padStart(inputREF.value)
 
+if (string === '') {
+      alert('Please put digit!');}
 
-
-  for (const el of string) {
-    if (string === '') { alert('ERROR') }
-    else {
+else { for (const el of string) {
       const divREF = document.createElement(`div`)
-      divREF.classList.add('color__boxes')
       divREF.style.width = `${divWidth}px`
       divREF.style.height = `${divHeight}px`
       divREF.style.backgroundColor = getRandomHexColor()
       divWidth += 10
       divHeight += 10
-
-
-      mainDivREF.append(divREF);
     
+      mainDivREF.append(divREF);
+      string = '';
+      inputREF.value = '';
 
-      destroyButREF.addEventListener('click', () => {
+    destroyButREF.addEventListener('click', () => {
         divREF.remove();
-        inputREF.value = ''
-      })
+        document.location.reload(true);})
     
     }
   }
@@ -50,6 +46,115 @@ createButREF.addEventListener('click', () => {
 
 
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// const mainDivREF = document.querySelector('#boxes')
+// const inputREF = document.querySelector('[type="number"]')
+// const createButREF = document.querySelector('[data-create]')
+// const destroyButREF = document.querySelector('[data-destroy]')
+
+// // console.log(inputREF.value);
+
+// let divWidth = 30;
+// let divHeight = 30;
+// const arr = []
+// let string = '';
+
+
+
+// createButREF.addEventListener('click', () => {
+
+//   string = string.padStart(inputREF.value)
+  
+//   for (const el of string) {
+//     const divREF = document.createElement(`div`)
+//     divREF.classList.add('color__boxes')
+//     divREF.style.width = `${divWidth}px`
+//     divREF.style.height = `${divHeight}px`
+//     divREF.style.backgroundColor = getRandomHexColor()
+//     divWidth += 10
+//     divHeight += 10
+
+
+//     mainDivREF.append(divREF);
+    
+
+
+//     destroyButREF.addEventListener('click', () => {
+//   divREF.remove();
+//   inputREF.value = '0'
+// })
+//   }
+// })
+
 
 
 
